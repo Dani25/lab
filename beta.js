8bddb4d6945e3dc74e84e43388fdbd382f23d355
Daniela Onita <danielaonita25@gmail.com>
2020-10-14 17:35:26 +0300

    Update beta.js

--- beta.js
+++ beta.js
@@ -19,7 +19,7 @@ function on_device_orientation(evt)
 	context.clearRect(0, 0, c.width, c.height);
 	context.fillStyle = ""#6A0888";
 	context.beginPath();
-	context.fillRect(180, 150, 180, event.beta);
+	context.fillRect(180, 150, 50, event.beta);
 
 	
 
