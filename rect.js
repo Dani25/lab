5bc5c88f28e762f9182136fcab39c92d846534f5
Daniela Onita <danielaonita25@gmail.com>
2020-10-14 11:34:00 +0300

    Add files via upload

---
+++ rect.js
@@ -0,0 +1,27 @@
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(evt)
+{
+	var alpha = evt.alpha;
+	var beta = evt.beta;
+	var gamma = evt.gamma;
+		
+	document.getElementById("a").innerHTML = "alpha = "+alpha;
+	document.getElementById("b").innerHTML = "beta = "+beta;
+	document.getElementById("c").innerHTML = "gamma = "+gamma;
+	
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+	var centru = {x:canvas.width/2, y:canvas.height/2}
+	var latura_patrat = 50;
+	
+
+
+	context.fillStyle = "#FF6600";
+	context.fillText("Beta: " + Math.round(event.beta), 10, 140);
+	context.beginPath();
+	context.fillRect(180, 150, event.beta, 90);
+
+	
+
+}
