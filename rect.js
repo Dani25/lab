7bdf69d64bf78a0386d1be82888317b188c0d147
Daniela Onita <danielaonita25@gmail.com>
2020-10-14 11:39:06 +0300

    Update rect.js

--- rect.js
+++ rect.js
@@ -17,7 +17,7 @@ function on_device_orientation(evt)
 	
 
 	context.clearRect(0, 0, c.width, c.height);
-	context.fillStyle = "#FF6600";
+	context.fillStyle = "#6A0888";
 	context.fillText("Beta: " + Math.round(event.beta), 10, 140);
 	context.beginPath();
 	context.fillRect(180, 150, event.beta, 90);
